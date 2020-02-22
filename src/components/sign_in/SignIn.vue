252f61a47ffa8c62db4d7f50919cd342a9b71086
João Marco Oliveira Barros <joaomarcobarros@usp.br>
2020-02-21 21:36:40 -0300

    Sign In page done

--- src/components/sign_in/SignIn.vue
+++ src/components/sign_in/SignIn.vue
@@ -1,6 +1,22 @@
 <template>
-  <div class="corpo">
-      
+  <div class="corpo flex">
+      <div class="sign-in-container flex">
+            <div class="side-information-sign-in flex-column wrap">
+                <img src="../../assets/images/desktop.png" alt="">
+                <h1>PediScripts</h1>
+                <h2>Clinical Documentation with efficiency</h2>
+            </div>
+            <div class="credentials-sign-in flex-column wrap">
+                <h1>Create an account</h1>
+                <input type="text" class="sign-in-input-email" placeholder="Email">
+                <input type="password" class="sign-in-input-password" placeholder="Password">
+                <button class="btn-solid-blue">Sign Up</button>
+                <div class="sing-in-redirection flex">
+                    <h3>Already have an account <span>Sign in</span></h3>
+                </div>
+                <button class="btn-outlined-blue">Use without an account</button>
+            </div>
+      </div>
   </div>
 </template>
 
