7e6c9a7c9577331bed33217d37746e10edd11d31
João Marco Oliveira Barros <joaomarcobarros@usp.br>
2020-02-22 00:58:18 -0300

    Sign Up page started

--- src/router/index.js
+++ src/router/index.js
@@ -1,29 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 import SignUp from '../views/SignUpView.vue'
+import SignIn from '../views/SignInView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/sign-in',
+    name: 'SignIn',
+    component: SignIn
+  },
+  {
     path: '/',
     name: 'SignUp',
     component: SignUp
   },
-  {
-    path: '/home',
-    name: 'Home',
-    component: Home
-  },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = new VueRouter({
---
+++ src/views/SignInView.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="corpo">
+      <sign-in/>
+  </div>
+</template>
+
+<script>
+import SignIn from "../components/sign_in/SignIn"
+export default {
+    components:{
+        SignIn,
+    }
+}
+</script>
+
+<style>
+
+</style>
