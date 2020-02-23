d055ca8a6110c1f2979f291adb872abeee2e4fd1
João Marco Oliveira Barros <joaomarcobarros@usp.br>
2020-02-23 19:22:42 -0300

    Header finished

--- src/components/navbar/MainHeader.vue
+++ src/components/navbar/MainHeader.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="top-page">
     <header class="flex">
+       <div class="left-space"></div>
       <div class="logo-container flex">
         <img id="logo-img" src="../../assets/images/logo.png" alt="logo-img" />
         <p class="logo-text">Pediscripts</p>
@@ -25,142 +26,208 @@
           <p class="sign-out-txt">Sign Out</p>
         </div>
       </div>
+      <div class="right-space"></div>
     </header>
 
     <div class="second-navbar">
-      <ul class="main-navigation">
-        <li>
-          <a href="#">Neuro/Card/Resp</a>
-          <ul>
-            <li>
-              <a href="#">Neurology</a>
-              <ul>
-                <li v-for="neuro in neuros" :key="neuro">
-                  <a href>{{neuro}}</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#">Cardiology</a>
-              <ul>
-                <li v-for="cardio in cardios" :key="cardio">
-                  <a href>{{cardio}}</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#">Respiratory</a>
-              <ul>
-                <li v-for="resp in resps" :key="resp">
-                  <a href>{{resp}}</a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href="#">GI/GU/Endo</a>
-          <ul>
-            <li>
-              <a href="#">Gastroenterology</a>
-              <ul>
-                <li v-for="gi in gis" :key="gi">
-                  <a href>{{gi}}</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#">Genitourinary/ Renal</a>
-              <ul>
-                <li v-for="gu in gus" :key="gu">
-                  <a href>{{gu}}</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href="#">Endocrinology</a>
-              <ul>
-                <li v-for="endo in endos" :key="endo">
-                  <a href>{{endo}}</a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href="#">HO/ID/Rheum</a>
-          <ul>
-            <li>
-              <a href>Hematology/Oncology</a>
-              <ul>
-                <li v-for="ho in hos" :key="ho">
-                  <a href>{{ho}}</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href>Infectious Disease</a>
-              <ul>
-                <li v-for="id in ids" :key="id">
-                  <a href>{{id}}</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href>Rheumatology</a>
-              <ul>
-                <li v-for="rheum in rheums" :key="rheum">
-                  <a href>{{rheum}}</a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href="#"> Derm/EENT/MSK/Surg</a>
-          <ul>
-            <li>
-              <a href>Dermatology</a>
-              <ul>
-                <li v-for="derm in derms" :key="derm">
-                  <a href>{{derm}}</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href>EENT</a>
-              <ul>
-                <li v-for="eent in eents" :key="eent">
-                  <a href>{{eent}}</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href>Trauma & Musculoskeletal </a>
-              <ul>
-                <li v-for="msk in msks" :key="msk">
-                  <a href>{{msk}}</a>
-                </li>
-              </ul>
-            </li>
-            <li>
-              <a href>Surgical Referrals</a>
-              <ul>
-                <li v-for="surg in surgs" :key="surg">
-                  <a href>{{surg}}</a>
-                </li>
-              </ul>
-            </li>
-          </ul>
-        </li>
-        <li>
-            <a href="">Psych</a>
+      <ul class="main-navigation flex">
+        <div class="left">
+          <li>
+            <a href="#">Neuro/Card/Resp</a>
             <ul>
-                 <li v-for="psych in psychs" :key="psych">
-                  <a href>{{psych}}</a>
-                </li>
+              <li>
+                <a href="#">Neurology</a>
+                <ul>
+                  <li v-for="neuro in neuros" :key="neuro">
+                    <a href>{{neuro}}</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Cardiology</a>
+                <ul>
+                  <li v-for="cardio in cardios" :key="cardio">
+                    <a href>{{cardio}}</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Respiratory</a>
+                <ul>
+                  <li v-for="resp in resps" :key="resp">
+                    <a href>{{resp}}</a>
+                  </li>
+                </ul>
+              </li>
             </ul>
-        </li>
+          </li>
+          <li>
+            <a href="#">GI/GU/Endo</a>
+            <ul>
+              <li>
+                <a href="#">Gastroenterology</a>
+                <ul>
+                  <li v-for="gi in gis" :key="gi">
+                    <a href>{{gi}}</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Genitourinary/ Renal</a>
+                <ul>
+                  <li v-for="gu in gus" :key="gu">
+                    <a href>{{gu}}</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href="#">Endocrinology</a>
+                <ul>
+                  <li v-for="endo in endos" :key="endo">
+                    <a href>{{endo}}</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">HO/ID/Rheum</a>
+            <ul>
+              <li>
+                <a href>Hematology/Oncology</a>
+                <ul>
+                  <li v-for="ho in hos" :key="ho">
+                    <a href>{{ho}}</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href>Infectious Disease</a>
+                <ul>
+                  <li v-for="id in ids" :key="id">
+                    <a href>{{id}}</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href>Rheumatology</a>
+                <ul>
+                  <li v-for="rheum in rheums" :key="rheum">
+                    <a href>{{rheum}}</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href="#">Derm/EENT/MSK/Surg</a>
+            <ul>
+              <li>
+                <a href>Dermatology</a>
+                <ul>
+                  <li v-for="derm in derms" :key="derm">
+                    <a href>{{derm}}</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href>EENT</a>
+                <ul>
+                  <li v-for="eent in eents" :key="eent">
+                    <a href>{{eent}}</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href>Trauma & Musculoskeletal</a>
+                <ul>
+                  <li v-for="msk in msks" :key="msk">
+                    <a href>{{msk}}</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href>Surgical Referrals</a>
+                <ul>
+                  <li v-for="surg in surgs" :key="surg">
+                    <a href>{{surg}}</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href>Psych</a>
+            <ul>
+              <li v-for="psych in psychs" :key="psych">
+                <a href>{{psych}}</a>
+              </li>
+            </ul>
+          </li>
+        </div>
+        <dev class="right">
+          <li>
+            <a href>Primary Care</a>
+            <ul>
+              <li>
+                <a href>General Pediatrics</a>
+                <ul>
+                  <li v-for="gp in gps" :key="gp">
+                    <a href>{{gp}}</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href>Adolescent</a>
+                <ul>
+                  <li v-for="adol in adols" :key="adol">
+                    <a href>{{adol}}</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href>Telephone Triage</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href>Emergency</a>
+            <ul>
+              <li v-for="emerg in emergs" :key="emerg">
+                <a href>{{emerg}}</a>
+              </li>
+            </ul>
+          </li>
+          <li>
+            <a href>Hospital Medicine</a>
+            <ul>
+              <li>
+                <a href>Hospital Medicine</a>
+                <ul>
+                  <li v-for="hm in hms" :key="hm">
+                    <a href>{{hm}}</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href>PICU</a>
+                <ul>
+                  <li v-for="picu in picus" :key="picu">
+                    <a href>{{picu}}</a>
+                  </li>
+                </ul>
+              </li>
+              <li>
+                <a href>NICU</a>
+                <ul>
+                  <li v-for="nicu in nicus" :key="nicu">
+                    <a href>{{nicu}}</a>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+          </li>
+        </dev>
       </ul>
     </div>
   </div>
@@ -201,10 +268,123 @@ export default {
       ids: ["ID PC", "Acute Illness Adol", "ID ED", "ID HM", "ID NICU"],
       rheums: ["Rheum PC", "Rheum Adol", "Rheum HM"],
       derms: ["Derm PC", "Derm Adol", "Derm ED", "Derm HM", "Derm NICU"],
-      eents:["EENT PC", "EENT Adol","EENT ED","EENT HM", "Ophoto PC", "Ophoto Adol", "Oral PC", "Oral Adol"],
-      msks:["MSK PC", "MSK Adol", "MSK ED", "MSK HM"],
-      surgs:["Surg PC", "Surg HM", "Post Op PICU"],
-      psychs:["Psych PC","Adol PC", "Dev PC", "Psych ED", "Psych HM"]
+      eents: [
+        "EENT PC",
+        "EENT Adol",
+        "EENT ED",
+        "EENT HM",
+        "Ophoto PC",
+        "Ophoto Adol",
+        "Oral PC",
+        "Oral Adol"
+      ],
+      msks: ["MSK PC", "MSK Adol", "MSK ED", "MSK HM"],
+      surgs: ["Surg PC", "Surg HM", "Post Op PICU"],
+      psychs: ["Psych PC", "Adol PC", "Dev PC", "Psych ED", "Psych HM"],
+      gps: [
+        "Cardiology PC",
+        "Dermatology PC",
+        "DVP PC",
+        "Endo PC",
+        "ENT PC",
+        "Genetic PC",
+        "GI PC",
+        "GU PC",
+        "Heme Onc PC",
+        "ID PC",
+        "Misc PC",
+        "MSK PC",
+        "Neurology PC",
+        "NewBorn PC",
+        "Nutrition PC",
+        "Ophtho PC",
+        "Oral PC",
+        "Psych PC",
+        "Resp PC",
+        "Rheum PC",
+        "Surgery PC",
+        "Telephone Triage PC",
+        "Wellcheck"
+      ],
+      adols: [
+        "Cardiology Adolescent",
+        "Dermatology Adolescent",
+        "DVP Adolescent",
+        "Endo Adolescent",
+        "ENT Adolescent",
+        "Genetic Adolescent",
+        "GI Adolescent",
+        "GU Female Adolescent",
+        "GU Male Adolescent",
+        "Heme Onc Adolescent",
+        "Neurology Adolescent",
+        "Nutrition Adolescent",
+        "Ophtho Adolescent",
+        "Oral Adolescent",
+        "Psych Adolescent",
+        "Resp Adolescent",
+        "Rheum Adolescent",
+        "Surgery Adolescent",
+        "Wellcheck Adolescent"
+      ],
+      emergs: [
+        "Cardiology ED",
+        "Dermatology ED",
+        "Endo ED",
+        "EENT ED",
+        "Genetic ED",
+        "GI ED",
+        "GU ED",
+        "Heme Onc ED",
+        "ID ED",
+        "MSK ED",
+        "Neurology ED",
+        "Nutrition ED",
+        "Ophtho ED",
+        "Oral ED",
+        "Psych ED",
+        "Resp ED",
+        "Rheum ED",
+        "Surgery ED",
+        "Toxicology ED"
+      ],
+      hms: [
+        "Cardiology HM",
+        "Dermatology HM",
+        "Endo HM",
+        "EENT HM",
+        "GI HM",
+        //"GU HM",
+        "Heme Onc HM",
+        "ID HM",
+        "MSK HM",
+        "Neurology HM",
+        "Newborn HM",
+        //"Nutrition HM",
+        //"Ophtho HM",
+        //"Oral HM",
+        "Psych HM",
+        "Respiratory HM",
+        "Renal HM",
+        "Rheum HM",
+        "Surgery HM"
+      ],
+      picus: [
+        "Cardiology PICU",
+        "Neurology PICU",
+        "MISC PICU",
+        "POSTOP PICU",
+        "PROC PICU",
+        "Respiratory PICU"
+      ],
+      nicus: [
+        "Basic NICU",
+        "Cardiology NICU",
+        "Dematology NICU",
+        "Neurology NICU",
+        "GI PICU",
+        "ID PICU"
+      ]
     };
   }
 };
