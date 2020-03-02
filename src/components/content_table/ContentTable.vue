f5232bff1998dfb80367e1042f5341dea25aeaee
João Marco Oliveira Barros <joaomarcobarros@usp.br>
2020-03-02 02:50:10 -0300

    template Library design fixed

--- src/components/content_table/ContentTable.vue
+++ src/components/content_table/ContentTable.vue
@@ -1,9 +1,12 @@
 <template>
   <div class="corpo flex-column">
     <div class="content-table-page flex-column">
-      <div class="content-table-container flex wrap">
-        <div class="content-table-item flex" v-for="content in contents" :key="content">
-          <h1>{{content}}</h1>
+      <div class="content-table-container flex-column">
+        <h1 class="title">Template Library</h1>
+        <div class="content-table-box flex wrap">
+          <div class="content-table-item flex" v-for="content in contents" :key="content">
+            <h1>{{content}}</h1>
+          </div>
         </div>
       </div>
     </div>
